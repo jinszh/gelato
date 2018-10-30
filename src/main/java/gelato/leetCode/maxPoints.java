1d9d106c0930c29dee38d535a0080364aeae99ea
Zhang <jszhang@ea.com>
2018-10-30 17:55:25 +0800

    udpate max points

--- src/main/java/gelato/leetCode/maxPoints.java
+++ src/main/java/gelato/leetCode/maxPoints.java
@@ -1,14 +1,15 @@
 package gelato.leetCode;
 
 import java.awt.*;
+import java.math.BigDecimal;
 
 public class maxPoints {
     public int maxPoints(Point[] points) {
-        Double [][] slope;
+        BigDecimal[][] slope;
         int n = 0;
         if(points != null && points.length > 0) {
-            n = 1;
-            slope = new Double[points.length][points.length];
+            n = points.length > 1 ? 2 : 1;
+            slope = new BigDecimal[points.length][points.length];
             boolean [][] contained = new boolean[points.length][points.length];
             for (int i = 0; i < points.length; i++) {
                 for (int j = i + 1; j < points.length; j++) {
@@ -16,28 +17,38 @@ public class maxPoints {
                         if(points[i].x == points[j].x){
                             slope[i][j] = null;
                         }else {
-                            slope[i][j] = Double.MAX_VALUE;
+                            slope[i][j] = BigDecimal.;
                         }
                     } else {
-                        slope[i][j] = (double) (points[i].x - points[j].x) / (points[i].y - points[j].y);
+                        slope[i][j] = BigDecimal.valueOf ((points[i].x - points[j].x) / (points[i].y - points[j].y));
                     }
                     contained[i][j] = false;
                 }
             }
             for(int i = 0; i < points.length - 1; i++){
+                int base = 1;
                 for(int j = i + 1; j < points.length; j++){
                     if(contained[i][j]){continue;}
-                    int cur = 2;
-                    for(int k = j + 1; k < points.length; k ++){
-                        if(slope[i][j] == null || slope[i][k] == null || Math.abs(slope[i][j]- slope[i][k]) < 0.0000000000001){
-                            cur ++;
-                            contained[j][k] = true;
+                    if(slope[i][j] == null){
+                        base++;
+                        if(base > n){
+                            n = base;
+                        }
+                       // contained[i][j] = true;
+                    }else {
+                        int cur = base + 1;
+                        for (int k = j + 1; k < points.length; k++) {
+                            if (slope[i][k] == null || slope[i][j].compareTo(slope[i][k]) == 0) {
+                                cur++;
+                       //         contained[j][k] = true;
+                            }
+                        }
+                        if(cur > n){
+                            n = cur;
                         }
                     }
-                    if(cur > n){
-                        n = cur;
-                    }
                 }
+
             }
         }
         return n;
