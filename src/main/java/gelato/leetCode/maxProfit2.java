f9913503d42b2cb539a7ef951a19a337510975ed
Zhang <jszhang@ea.com>
2018-11-06 17:52:48 +0800

    update maxProfit2

--- src/main/java/gelato/leetCode/maxProfit2.java
+++ src/main/java/gelato/leetCode/maxProfit2.java
@@ -2,50 +2,58 @@ package gelato.leetCode;
 
 import gelato.util.Util;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class maxProfit2 {
     public int maxProfit(int k, int[] prices) {
-        Util.checkTime();
         int v = 0;
         if (k > 0 && prices.length > 0) {
-            int[][][] memo = new int[k][prices.length][prices.length];
-            int[][] max = new int[prices.length][prices.length];
-            int[] sellPoint = new int[prices.length];
-            int highe = prices[0];
-            for (int i = 0; i < prices.length - 1; i++) {
-                max[i][i] = 0;
-                Integer lowe = prices[i];
-                if(i > 0 && prices[i] <= prices[i-1]){
-                    sellPoint[i] = highe;
-                }else{
-                    sellPoint[i] = highe = prices[i];
+            List<Integer> buy = new ArrayList<>();
+            List<Integer> sell = new ArrayList<>();
+            int rl = 0, rd = 0;
+            for (int i = 0; i < prices.length; i++) {
+                if (i < prices.length - 1 && prices[i] == prices[i + 1]) {
+                    continue;
                 }
-                for (int j = i + 1; j < prices.length; j++) {
-                    max[i][j] = 0;
-                    if (prices[j] < lowe) {
-                        lowe = prices[j];
-                    }
-                    max[i][j] = Math.max(max[i][j - 1], prices[j] - lowe);
+                if (i > 0 && prices[i] > prices[rl] && ((i < prices.length - 1 && prices[i] > prices[i + 1]) || i == prices.length - 1)) {
+                    sell.add(i);
                 }
+                if ((i == 0 || (i > 0 && prices[i] < prices[rl])) && i < prices.length - 1 && prices[i] < prices[i + 1]) {
+                    buy.add(i);
+                }
+                rl = i;
             }
-            Util.checkTime();
-            long loop = 0;
-            for (int n = 0; n < k; n++) {
-                for (int i = 0; i < prices.length - 1; i++) {
-                    memo[n][i][i] = 0;
-                    for (int j = i + 1; j < prices.length; j++) {
-                        memo[n][i][j] = Math.max(memo[n][i][j - 1], (n > 0 ? memo[n - 1][i][j] : max[i][j]));
-                        if (n > 0 && prices[j] >= sellPoint[j]) {
-                            for (int sep = i + 1; sep < j - 1; sep++) {
-                                memo[n][i][j] = Math.max(max[i][sep] + memo[n - 1][sep + 1][j], memo[n][i][j]);
-                                loop++;
+            if (buy.size() > 0 && sell.size() > 0) {
+                int[][] max = new int[buy.size()][sell.size()];
+                for (int i = 0; i < buy.size(); i++) {
+                    max[i][i] = prices[sell.get(i)] - prices[buy.get(i)];
+                    int high = prices[sell.get(i)];
+                    for (int j = i + 1; j < sell.size(); j++) {
+                        if (prices[sell.get(j)] > high) {
+                            high = prices[sell.get(j)];
+                        }
+                        max[i][j] = high - prices[buy.get(i)];
+                    }
+                }
+
+                int[][][] memo = new int[k][buy.size()][sell.size()];
+                for (int n = 0; n < k; n++) {
+                    for (int i = 0; i < buy.size(); i++) {
+                        memo[n][i][i] = max[i][i];
+                        for (int j = i + 1; j < sell.size(); j++) {
+                            memo[n][i][j] = Math.max(memo[n][i][j - 1], (n > 0 ? memo[n - 1][i][j] : max[i][j]));
+                            if (n > 0) {
+                                for (int sep = i; sep < buy.size() - 1 &&  sep < j; sep++) {
+                                    memo[n][i][j] = Math.max(max[i][sep] + memo[n - 1][sep + 1][j], memo[n][i][j]);
+                                }
                             }
                         }
                     }
                 }
+                v = memo[k - 1][0][sell.size() - 1];
             }
-            Util.print("loop is " + loop);
-           t Util.checkTime();
-            v = memo[k - 1][0][prices.length - 1];
         }
         return v;
     }
