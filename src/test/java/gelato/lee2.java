550f5ee10b964667216deb5f83b0168f74ec2d9b
Zhang <jszhang@ea.com>
2018-11-26 14:06:27 +0800

    t

--- src/test/java/gelato/lee2.java
+++ src/test/java/gelato/lee2.java
@@ -226,9 +226,9 @@ public class lee2 extends LeetTests {
     public void testTrappingRainWater3D() {
         TrappingRainWater3D f = new TrappingRainWater3D();
         int s = f.trapRainWater(Util.getTwoDMatrix("[\n" +
-                "  [1,4,3,1,3,2],\n" +
-                "  [3,2,1,3,2,4],\n" +
-                "  [2,3,3,2,3,1]\n" +
+                "  [3,3,3,3,3,3],\n" +
+                "  [3,3,3,3,2,3],\n" +
+                "  [3,3,3,3,1,3]\n" +
                 "]"));
         Util.print(s);
     }
