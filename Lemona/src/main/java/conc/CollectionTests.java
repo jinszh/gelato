f0de60ac969e449a55265c11c44e52022ccfb334
jszhang <jszhang@ea.com>
2019-04-02 12:55:55 +0800

    some udpate

--- Lemona/src/main/java/conc/CollectionTests.java
+++ Lemona/src/main/java/conc/CollectionTests.java
@@ -2,10 +2,26 @@ package conc;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 public class CollectionTests {
-    public void t1(){
+    public void t1() {
         HashMap<Integer, Integer> hashSet = new HashMap<Integer, Integer>();
         hashSet.clone();
+    }
+
+    public void bl() {
+        BlockingQueue<Integer> blockingQueue = new ArrayBlockingQueue<>();
+
+
+        new Thread(() -> {
+            try {
+                blockingQueue.put(10);
+            } catch (InterruptedException e) {
+                ;
+            }
+        }).start();
+
     }
 }
