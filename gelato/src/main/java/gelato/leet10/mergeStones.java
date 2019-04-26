f7b44a886a80ff8e4a8a12f578da545d075499d5
Zhang <jszhang@ea.com>
2019-04-26 12:00:21 +0800

    2

--- gelato/src/main/java/gelato/leet10/mergeStones.java
+++ gelato/src/main/java/gelato/leet10/mergeStones.java
@@ -1,13 +1,23 @@
 package gelato.leet10;
 
 public class mergeStones {
+    int [][] dp;
     public int mergeStones(int[] stones, int K) {
-        int [][] dp = new int[stones.length][K + 1];
-        mergeStones(stones, 0, stones.length - 1, K);
-        return 0;
+        dp = new int[stones.length][K + 1];
+        merge(stones, 0, stones.length - 1, K);
+        return dp[stones.length - 1][K];
     }
 
-    private void merge(int [] stones, int b, int e, int K){
+    private int merge(int [] stones, int b, int e, int K) {
+        if (dp[e][K] > 0) return dp[e][K];
+        if (e + 1 >= K) {
+            int prod = 1;
+            for (int i = e - K + 1; i <= e; i++) {
+                prod *= stones[i];
+            }
+            return Math.min(merge(stones, b, e - K, K) * prod, merge(stones, b, e - 1, K))
+        } else {
 
+        }
     }
 }
